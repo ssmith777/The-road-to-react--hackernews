a51246fb59d15439d2d222c203766acbda999f54
ssmith <shawn.t.smith.777@gmail.com>
2019-01-17 11:29:06 -0600

    staging refactor 1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const DEFAULT_QUERY = 'React';
-const PATH_BASE = 'https://hn.algolia.com/api/v1';
-const PATH_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-const PARAM_HPP = 'hitsPerPage=';
-const DEFAULT_HPP = '10';
+// const DEFAULT_QUERY = 'React';
+// const PATH_BASE = 'https://hn.algolia.com/api/v1';
+// const PATH_SEARCH = '/search';
+// const PARAM_SEARCH = 'query=';
+// const PARAM_PAGE = 'page=';
+// const PARAM_HPP = 'hitsPerPage=';
+// const DEFAULT_HPP = '10';
+
+
+import { 
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+ } from './constants';
 
   
 class App extends Component {
