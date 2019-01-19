16e3b63153264abfd4c3268f1df6e3768230ac8f
ssmith <shawn.t.smith.777@gmail.com>
2019-01-19 14:16:21 -0600

    added fontawesome package and icon for the loading constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,10 @@
 import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
+library.add(faSpinner)
+//faSpinner.icon("50px")
 export const DEFAULT_QUERY = 'React';
 export const PATH_BASE = 'https://hn.algolia.com/api/v1';
 export const PATH_SEARCH = '/search';
@@ -7,5 +12,10 @@ export const PARAM_SEARCH = 'query=';
 export const PARAM_PAGE = 'page=';
 export const PARAM_HPP = 'hitsPerPage=';
 export const DEFAULT_HPP = '10';
-export const Loading = () => 
-<div>Loading...</div>
+
+
+export const Loading = () => (
+    <div>
+      loading... <FontAwesomeIcon size="2x" icon="spinner" />
+    </div>
+  )
