031b3f8742600b18ab454e89a67f2716b5192cea
ssmith <shawn.t.smith.777@gmail.com>
2019-01-19 12:06:38 -0600

    Added to Ref a DOM Element, change Search to Class to access Ref/Dom element and set focus

--- src/Search/index.js
+++ src/Search/index.js
@@ -1,24 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './index.css';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
 
-const Search = ({value, onChange, onSubmit, children }) => 
-  <form onSubmit={onSubmit}>
-    <input
-      type="text"
-      value={value}
-      onChange={onChange}
-      />
-      <button type="submit">
-        {children}
-      </button>
-  </form>
+// ES6 class component
+class Search extends Component {
+  componentDidMount() {
+    if (this.input){
+      this.input.focus();       
+    }
+  }
+  render() {
+    const {
+      value,
+      onChange,
+      onSubmit,
+      children 
+    } = this.props;
 
-Search.prototype = {
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onSubmit: PropTypes.func.isRequired,
-  children: PropTypes.node.isRequired
-};
+    return (
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          ref={el => this.input =el}
+          />
+          <button type="submit">
+            {children}
+          </button>
+      </form>
+      );
+  }
+}
+
 
 export default Search;
+
+// functional stateless component 
+// Example: with ref example
+// note: no lifecycle method so we can not trigger the focus
+//------------------------------------
+// const Search = ({
+//     value,
+//     onChange,
+//     onSubmit,
+//     children 
+//   }) => {
+//   let input;
+//   return (
+//     <form onSubmit={onSubmit}>
+//       <input
+//         type="text"
+//         value={value}
+//         onChange={onChange}
+//         ref={el => input = el}
+//         />
+//         <button type="submit">
+//           {children}
+//         </button>
+//     </form>
+//   );
+// }
+
+
+// used for Proptypes but commited out because we changed to a class to focus on input field 
+// Search.prototype = {
+//   value: PropTypes.string.isRequired,
+//   onChange: PropTypes.func.isRequired,
+//   onSubmit: PropTypes.func.isRequired,
+//   children: PropTypes.node.isRequired,
+// };
