bde2ff934855876e6961a651b1b4789c4ec69ae6
ssmith <shawn.t.smith.777@gmail.com>
2019-09-02 17:14:24 -0500

    Updates

---
+++ precache-manifest.03cd07de32fddbbe32f797a5cdfbfb7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2581a2e37ac5b048a321",
+    "url": "/The-road-to-react--hackernews/static/css/main.ba477acf.chunk.css"
+  },
+  {
+    "revision": "2581a2e37ac5b048a321",
+    "url": "/The-road-to-react--hackernews/static/js/main.2581a2e3.chunk.js"
+  },
+  {
+    "revision": "b96698164c88cfa0d4ae",
+    "url": "/The-road-to-react--hackernews/static/js/1.b9669816.chunk.js"
+  },
+  {
+    "revision": "8148730c16da9772e422",
+    "url": "/The-road-to-react--hackernews/static/js/runtime~main.8148730c.js"
+  },
+  {
+    "revision": "71b3fedfab27859e9adad63ec6d0fd39",
+    "url": "/The-road-to-react--hackernews/index.html"
+  }
+];
